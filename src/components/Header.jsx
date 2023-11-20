902c5d567681da1c72eee699d1fb4a2471eb1807
Japheth Awah <junery115@gmail.com>
2023-11-20 13:10:14 +0100

    created the spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Header = () => {
 
@@ -25,7 +25,7 @@ const Header = () => {
     }
   }
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img src ='https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg' alt="logo" 
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,10 @@
 import { Outlet, Navigate } from "react-router";
 import { useAuthStatus } from "../hooks/useAuthStatus";
+import Spinner from "./Spinner";
 const PrivateRoute = () => {
   const {loggedIn, checkingStatus } = useAuthStatus()
   if(checkingStatus){
-    return <h3>Loading...</h3>
+    return <h3><Spinner /></h3>
   }
      return loggedIn ? <Outlet /> : 
       <Navigate to= "/sign-in" />;
