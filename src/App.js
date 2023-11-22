be623d8b5e7cbb1b4a70d48c1258658de971a8fe
Japheth Awah <junery115@gmail.com>
2023-11-22 11:58:05 +0100

    created the create listing page UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from './pages/CreateListing';
+import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
+import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import PrivateRoute from "./components/PrivateRoute"
-import ForgotPassword from "./pages/ForgotPassword";
-import Offers from "./pages/Offers";
-import Header from './components/Header';
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
           <Route path="/sign-up" element = { <SignUp />} />
           <Route path="/forgot-password" element = { <ForgotPassword />} />
           <Route path="/offers" element = { <Offers />} />
+          <Route path="/create-listing" element = { <CreateListing />} />
         </Routes> 
       </Router>
            
